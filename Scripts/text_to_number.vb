880a135156181f5e99b5c28ba479a79211c3ddfe
Joao Roldi <joao.roldi@gmail.com>
2019-12-10 09:46:14 -0300

    Code improvement

--- Scripts/text_to_number.vb
+++ Scripts/text_to_number.vb
@@ -1,35 +1,12 @@
 Sub text_to_number()
 ' This is a stupid macro to replace those stupid numbers in text format
 
-    Selection.Replace What:="0", Replacement:="0", LookAt:=xlPart, _
+Let x = 0
+
+Do while x < 10:
+    Selection.Replace What:=Cstr(x), Replacement:=Cstr(x), LookAt:=xlPart, _
         SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
         ReplaceFormat:=False
-    Selection.Replace What:="1", Replacement:="1", LookAt:=xlPart, _
-        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="2", Replacement:="2", LookAt:=xlPart, _
-        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="3", Replacement:="3", LookAt:=xlPart, _
-        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="4", Replacement:="4", LookAt:=xlPart, _
-        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="5", Replacement:="5", LookAt:=xlPart, _
-        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="6", Replacement:="6", LookAt:=xlPart, _
-        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="7", Replacement:="7", LookAt:=xlPart, _
-        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="8", Replacement:="8", LookAt:=xlPart, _
-        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="9", Replacement:="9", LookAt:=xlPart, _
-        SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-        
+    x = x +1
+Loop
 End Sub
