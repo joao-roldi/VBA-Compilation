44fff05a8ebb97d1a49e393174ebcb680c1bce6f
Joao Roldi <joao.roldi@gmail.com>
2019-12-11 11:57:32 -0300

    Bug Fix
    Changed date to stupid american format (mm/dd/yyyy)

--- Scripts/months_in_text_to_date.vb
+++ Scripts/months_in_text_to_date.vb
@@ -1,40 +1,15 @@
 Sub monhts_in_text_to_date()
-    ActiveCell.Columns("A:A").EntireColumn.Select
-
-    Selection.Replace What:="jan", Replacement:="01/01/2019", LookAt:=xlPart _
-        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="fev", Replacement:="01/02/2019", LookAt:=xlPart _
-        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="mar", Replacement:="01/03/2019", LookAt:=xlPart _
-        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="abr", Replacement:="01/04/2019", LookAt:=xlPart _
-        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="mai", Replacement:="01/05/2019", LookAt:=xlPart _
-        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="jun", Replacement:="01/06/2019", LookAt:=xlPart _
-        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="jul", Replacement:="01/07/2019", LookAt:=xlPart _
-        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="ago", Replacement:="01/08/2019", LookAt:=xlPart _
-        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="set", Replacement:="01/09/2019", LookAt:=xlPart _
-        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="out", Replacement:="01/10/2019", LookAt:=xlPart _
-        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="nov", Replacement:="01/11/2019", LookAt:=xlPart _
-        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
-    Selection.Replace What:="dez", Replacement:="01/12/2019", LookAt:=xlPart _
-        , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, _
-        ReplaceFormat:=False
+    Columns("C:C").Select
+    Selection.Replace What:="jan", Replacement:="01/01/2019", LookAt:=xlPart , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+    Selection.Replace What:="fev", Replacement:="02/01/2019", LookAt:=xlPart , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+    Selection.Replace What:="mar", Replacement:="03/01/2019", LookAt:=xlPart , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+    Selection.Replace What:="abr", Replacement:="04/01/2019", LookAt:=xlPart , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+    Selection.Replace What:="mai", Replacement:="05/01/2019", LookAt:=xlPart , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+    Selection.Replace What:="jun", Replacement:="06/01/2019", LookAt:=xlPart , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+    Selection.Replace What:="jul", Replacement:="07/01/2019", LookAt:=xlPart , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+    Selection.Replace What:="ago", Replacement:="08/01/2019", LookAt:=xlPart , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+    Selection.Replace What:="set", Replacement:="09/01/2019", LookAt:=xlPart , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+    Selection.Replace What:="out", Replacement:="10/01/2019", LookAt:=xlPart , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+    Selection.Replace What:="nov", Replacement:="11/01/2019", LookAt:=xlPart , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
+    Selection.Replace What:="dez", Replacement:="12/01/2019", LookAt:=xlPart , SearchOrder:=xlByRows, MatchCase:=False, SearchFormat:=False, ReplaceFormat:=False
 End Sub
