425e7be11cde991a3e925a831d23ea941662cc54
Joao Roldi <joao.roldi@gmail.com>
2020-04-13 09:08:32 -0300

    Organize Worksheets Alphabetically

---
+++ Scripts/organize_worksheets.vb
@@ -0,0 +1,15 @@
+Sub SortWorksheetsTabs()
+Application.ScreenUpdating = False
+Dim ShCount As Integer, i As Integer, j As Integer
+ShCount = Sheets.Count
+
+For i = 1 To ShCount - 1
+    For j = i + 1 To ShCount
+        If UCase(Sheets(j).Name) < UCase(Sheets(i).Name) Then
+            Sheets(j).Move before:=Sheets(i)
+        End If
+    Next j
+Next i
+
+Application.ScreenUpdating = True
+End Sub
